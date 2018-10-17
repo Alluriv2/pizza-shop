0e468931314629695db66e80fec465ac67df2c94
Alluri <alluriv2@nationwide.com>
2018-10-17 14:46:47 -0400

    initial commit

---
+++ src/main/java/com/example/demo/PizzaController.java
@@ -0,0 +1,17 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.Environment; 
+
+@RestController
+public class PizzaController{
+    
+    @RequestMapping("/pizzas")
+    public String getPizzas() {
+        return "Pizza are good";
+    }
+}
